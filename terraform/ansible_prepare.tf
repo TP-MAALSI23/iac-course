890de9ef744d571fe6fb3190051892972d9d99ed
Willem Houm <willem.houm@golem.ai>
2023-12-12 15:43:37 +0100

    bette SSH availability check

--- terraform/ansible_prepare.tf
+++ terraform/ansible_prepare.tf
@@ -13,7 +13,6 @@ resource "null_resource" "ansible_inventory" {
   provisioner "local-exec" {
     command = <<EOT
       echo '${templatefile("${path.module}/templates/ansible_inventory.tpl", { vm_instances = local.vm_instances_json, private_key_path = var.private_key_path, default_user = var.default_user })}' > ../ansible/inventory.yaml
-      ansible-playbook -i ../ansible/inventory.yaml ../ansible/playbook.yaml
     EOT
   }
 }
@@ -23,6 +22,24 @@ resource "null_resource" "ansible_playbook_exec" {
 
   provisioner "local-exec" {
     command = <<EOT
+      timeout=300  # Set your desired timeout in seconds
+      start_time=$(date +%s)
+
+      for ip in $(terraform output -json instance_ips | jq -r '.[]'); do
+        until ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i /Users/willemhoum/devspace/maalsi/cesi $ip "echo SSH is ready"; do
+            echo "Waiting for SSH port to be available on $ip..."
+            sleep 5
+
+            current_time=$(date +%s)
+            elapsed_time=$((current_time - start_time))
+
+            if [ $elapsed_time -ge $timeout ]; then
+                echo "Timeout reached. SSH server not ready within $timeout seconds."
+                exit 1
+            fi
+        done
+      done
+      
       ansible-playbook -i ../ansible/inventory.yaml ../ansible/playbook.yaml
     EOT
   }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,3 +4,10 @@ output "ssh_commands" {
     "ssh ${var.default_user}@${instance.network_interface[0].access_config[0].nat_ip} -i ${var.private_key_path}"
   ]
 }
+
+output "instance_ips" {
+  value = [
+    for instance in google_compute_instance.vm :
+    instance.network_interface[0].access_config[0].nat_ip
+  ]
+}
