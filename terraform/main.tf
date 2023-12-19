7e4dc8d07b485f3b6b729c49e73bc6a6bff26936
BSevault <b.sevault@gmail.com>
2023-12-19 08:53:17 +0000

    change instance var to one object

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,15 +1,18 @@
 # main.tf
 resource "google_compute_instance" "vm" {
-  count = length(var.instances)
+  # count = length(var.instances)
+  count = 3
 
-  name         = var.instances[count.index].name
+  name         = "${var.instance.name}-${count.index}"
   machine_type = var.machine_type
   zone         = var.zone
 
   boot_disk {
     initialize_params {
-      image = var.instances[count.index].image
-      size  = var.instances[count.index].disk_size
+      # image = var.instances[count.index].image
+      # size  = var.instances[count.index].disk_size
+      image = var.instance.image
+      size  = var.instance.disk_size
     }
   }
 
